08f18426068691538d49990dfea6e2a1928e70eb
Kevin <khvilaboa@gmail.com>
2020-11-11 00:28:37 +0100

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='qrplots',
+    version='',
+    packages=[''],
+    package_dir={'': 'qrplots'},
+    url='',
+    license='',
+    author='Kevin Henares',
+    author_email='khvilaboa@gmail.com',
+    description='',
+    install_requires=[
+          'qrcode[pil]',
+      ],
+)
